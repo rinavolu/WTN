efef1871ae2cdc730b1c79e0d4b7370443b063e5
Ravindra <Ravindra@DESKTOP-RJJQ3MM>
2020-06-12 13:52:14 +0530

    Exercise:-2 15th problem

---
+++ WTN/FlowControlStatements/com/wipro/flowcontrolstatements/Ex_15_SumOfAllDigitsOfTheGivenNumber.java
@@ -0,0 +1,19 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex_15_SumOfAllDigitsOfTheGivenNumber {
+
+	public static void main(String args[])
+	{
+		Integer n=Integer.parseInt(args[0]);
+	    int r,sum=0;
+	    while(n!=0)
+	    {
+	    	r=n%10;
+	    	sum=sum+r;
+	    	n=n/10;
+	    }
+	    System.out.println(sum);
+	}
+	
+}
+
