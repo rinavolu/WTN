be9a242693f9fd6f19521ac7545c8c85ca9b8341
Ravindra <Ravindra@DESKTOP-RJJQ3MM>
2020-06-12 11:21:04 +0530

    Exercise:-1 3rd Problem

---
+++ WTN/LanguageBasics/com/wipro/languagebasics/Ex_3_SumOfTwoNumbers.java
@@ -0,0 +1,12 @@
+package com.wipro.languagebasics;
+
+public class Ex_3_SumOfTwoNumbers {
+
+	public static void main(String args[])
+	{
+		int a,b;
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		System.out.printf("The sum of %d and %d is %d",a,b,a+b);
+	}
+}
