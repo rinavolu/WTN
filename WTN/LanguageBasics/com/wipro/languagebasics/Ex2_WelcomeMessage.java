3b5a44eb0e1c1b9ca7bb4d7c6ac940287354d5f3
Ravindra <Ravindra@DESKTOP-RJJQ3MM>
2020-06-12 11:17:28 +0530

    Execise:-1 2nd Problem

---
+++ WTN/LanguageBasics/com/wipro/languagebasics/Ex2_WelcomeMessage.java
@@ -0,0 +1,10 @@
+package com.wipro.languagebasics;
+
+public class Ex2_WelcomeMessage {
+	public static void main(String args[])
+	{
+		String str=args[0];
+		System.out.println("Welcome "+str);
+	}
+
+}
