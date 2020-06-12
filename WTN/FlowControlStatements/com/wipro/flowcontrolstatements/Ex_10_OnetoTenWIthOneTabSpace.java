83658c43f5834b1b6e44dc27d01952f99065dfd6
Ravindra <Ravindra@DESKTOP-RJJQ3MM>
2020-06-12 13:21:42 +0530

    Exercise:-2 10th problem

---
+++ WTN/FlowControlStatements/com/wipro/flowcontrolstatements/Ex_10_OnetoTenWIthOneTabSpace.java
@@ -0,0 +1,15 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex_10_OnetoTenWIthOneTabSpace {
+   
+	public static void main(String args[])
+	{
+		for(int i=1;i<=10;i++)
+		{
+			System.out.print(i);
+			if(i!=10)
+				System.out.print(" ");
+		}
+	}
+	
+}
