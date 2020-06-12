3d77c8b96c45a5eb4f19a4ee1235a8329f6bbd32
Ravindra <Ravindra@DESKTOP-RJJQ3MM>
2020-06-12 11:08:44 +0530

    Exercise:-1

---
+++ WTN/LanguageBasics/com/wipro/languagebasics/Ex_1_FormatTwoStrings.java
@@ -0,0 +1,10 @@
+package com.wipro.languagebasics;
+
+public class Ex_1_FormatTwoStrings {
+	public static void main(String args[])
+	{
+		String str1 = args[0];
+		String str2 = args[1];
+		System.out.println(str1+" Technologies "+str2);
+	}
+}
