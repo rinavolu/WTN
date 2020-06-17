ed12ce932b0e5d99bbaa3ac4ad8217a80f4eba56
ravindra1111 <54111248+ravindra1111@users.noreply.github.com>
2020-06-17 12:38:07 +0530

    Exercise:-3 9th problem

--- WTN/Arrays/com/wipro/arrays/Ex_9_NumberTenRemoval.java
+++ WTN/Arrays/com/wipro/arrays/Ex_9_NumberTenRemoval.java
@@ -4,17 +4,18 @@ public class Ex_9_NumberTenRemoval {
 
 	public static void main(String args[])
 	{
-		int a[]= {1, 10, 10, 2};
+		int a[]= {10, 11, 10, 2};
 		int n=a.length;
 		for(int i=0;i<n;i++)
 		{
-			if(a[i]==10)
+			if(a[i]==10||a[i]==0)
 				for(int j=i+1;j<n;j++)
 				{
-					if(a[j]!=10)
+					if(a[j]!=10&&a[j]!=0)
 					{
 						a[i]=a[j];
 						a[j]=0;
+						break;
 					}
 				}
 			if(a[i]==10)
